ee43b500ef25c744f4e351c2fb92a1b800818b3c
jb <herold.jb@gmail.com>
2022-12-04 08:40:02 +0100

    .

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ typologie = "Maison"
 
 df_bdd_return =  func.requesting_bdd(commune, typologie, our_host, our_dbname, our_user, our_password, auth_plugin)
 result = func.model_passing(df_bdd_return, surface)
-df_bdd = df_bdd_return.to_dict('index')
+df_bdd = df_bdd_return.to_dict('records')
 print({"transactions": df_bdd, "result": result})
 
 '''
