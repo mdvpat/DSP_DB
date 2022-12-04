5b5ed266d4f6d358698a11e596624f8f94910de3
jb <herold.jb@gmail.com>
2022-12-04 07:52:32 +0100

    .

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ async def post_param(param: Parameter): #token: str = Depends(oauth2_scheme)
   #surface = int(param.surface)
   df_bdd_return = func.requesting_bdd(param.commune, param.typologie, our_host, our_dbname, our_user, our_password, auth_plugin)
   a = param.surface+100
-  result = func.model_passing(df_bdd_return, param.surface)
+  #result = func.model_passing(df_bdd_return, param.surface)
   df_bdd = df_bdd_return.to_dict('index')
   return {"transactions": df_bdd, "result": a}
 
